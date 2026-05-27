f00a6c80134161fe1709249ecacab7770b64fa15
viii-fn <irhayeelvisebah@gmail.com>
2026-05-27 20:04:07 +0100

    tested syntax and memories

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState<number>(0);
 
-  const languages = [
-    { "title": "JavaScript", "id": 1 },
-    { "title": "TypeScript", "id": 2 },
-    { "title": "C#", "id": 2 },
-    { "title": "Go", "id": 3 },
-    { "title": "C++", "id": 4 },
-    { "title": "Rust", "id": 5 },
-  ];
-
-  function langList() {
+  function LangList() {
+    const languages = [
+      { "title": "JavaScript", "id": 1 },
+      { "title": "TypeScript", "id": 2 },
+      { "title": "C#", "id": 2 },
+      { "title": "Go", "id": 3 },
+      { "title": "C++", "id": 4 },
+      { "title": "Rust", "id": 5 },
+    ];
     
+    return (
+      languages.map(language =>
+        <li key={language.id}>
+          {language.title}
+        </li>
+      )
+    );
   }
 
   return (
@@ -22,6 +28,7 @@ function App() {
       <button className="counter" onClick={() => setCount(count + 1)}>
         Count Button <br /> Number of times clicked = {count}
       </button>
+      <LangList />
     </>
   )
 }
