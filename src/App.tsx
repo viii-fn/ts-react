b5ee4147fa2dc5c0b92eaddcb82b17e0294da7a8
viii-fn <irhayeelvisebah@gmail.com>
2026-06-17 03:33:27 +0100

    ###

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
 
 function App() {
+  interface Task {
+    id: number,
+    title: string
+  }
+  interface TaskListProps {
+    tasks: Task[]
+  }
+  const TaskList: TaskListProps[] = [];
 
+  return (
+    
+  )
 }
-
 export default App
